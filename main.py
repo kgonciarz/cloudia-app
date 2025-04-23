125c83342611c4a775b033809290acf7977086c8
kgonciarz <kgonciarz@cocoasource.ch>
2025-04-23 17:42:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,6 @@ if delivery_file:
         st.stop()
 
     exporter_name = uploaded_df['exporter'].dropna().astype(str).iloc[0]
-    st.success(f"Exporter from file: {exporter_name}")
 
     expected_columns = ['cooperative name', 'export lot n°/connaissement', 'date of purchase from cooperative',
                         'certification', 'farmer_id', 'farm_id', 'net weight (kg)', 'exporter']
