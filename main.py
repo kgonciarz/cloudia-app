fd45e88d9b5d26b4495bbcd66b44f94c4b7281e7
kgonciarz <kgonciarz@cocoasource.ch>
2025-04-25 14:42:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ if delivery_file:
 
     if not exceeded_df.empty:
         st.warning("These farmers have exceeded their quota:")
-        st.dataframe(exceeded_df[['farmer_id', 'net_weight_kg', 'max_quota_kg', 'quota_used_pct']])
+        st.dataframe(exceeded_df[['farmer_id', 'total_net_weight_kg', 'max_quota_kg', 'quota_used_pct']])
 
     st.write("### Quota Overview")
     st.dataframe(quota_df[['farmer_id', 'max_quota_kg', 'total_net_weight_kg', 'quota_used_pct', 'quota_status']])
