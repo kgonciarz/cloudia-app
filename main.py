a8484b01d1b3472987f652c226dfe3654c8262fc
kgonciarz <kgonciarz@cocoasource.ch>
2025-06-19 12:12:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def save_delivery_to_supabase(df):
     df_cleaned['farmer_id'] = df_cleaned['farmer_id'].str.strip().str.lower()
     df_cleaned['purchase_date'] = df_cleaned['purchase_date'].fillna(datetime.today().strftime('%Y-%m-%d'))
     # Zamień 'N/A' (i inne puste warianty) w certification na None
-    df_cleaned['certification'] = df_cleaned['certification'].replace(['N/A', 'n/a', 'na', 'NA'], None)
+    df_cleaned['certification'] = df_cleaned['certification'].replace(['N/A', 'n/a', 'na', 'NA'], '')
 
 
     def excel_date_to_date(excel_date):
